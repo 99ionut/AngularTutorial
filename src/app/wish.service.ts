961e30aed6ed58f2c7c196adea62c89d105b9cfd
Ionut Suciu <suciuionut888@gmail.com>
2024-03-12 11:28:43 +0100

    POST and GET http with parameters

--- src/app/wish.service.ts
+++ src/app/wish.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { WishItem } from '../shared/models/wishItem';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +9,33 @@ export class WishService {
 
   constructor(private http: HttpClient) { }
 
+  private getStandardOptions() : any {
+    return {
+      headers: new HttpHeaders({
+        "Content-type": "json",
+        "id": "2",
+      })
+    }
+  }
+
   getWishes(){
-    return this.http.get("assets/wishes.json");
+    let options = this.getStandardOptions();
+
+    return this.http.get("assets/wishes.json", options);
+    //get example with params
+    //return this.http.get("assets/wishes.json?format=json&id=2", options);
+    //or
+    options.params = new HttpParams({fromObject:{
+      format: "json",
+      id: "2"
+    }})
+  }
+
+  AddWish(wish: WishItem){
+    let options = this.getStandardOptions();
+
+    options.headers = options.headers.set("authorization","value for auth");
+    this.http.post("assets/wishes.json", wish ,options);
   }
 
 }
