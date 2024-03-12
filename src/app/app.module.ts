68f8cb67a83388acc95f22d824fae8d1814c648f
Ionut Suciu <suciuionut888@gmail.com>
2024-03-12 12:34:22 +0100

    converting wish to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,19 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { WishListComponent } from './wish-list/wish-list.component';
-import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
-import { WishFilterComponent } from './wish-filter/wish-filter.component';
-import { WishListItemComponent } from './wish-list-item/wish-list-item.component';
-import { HttpClientModule } from '@angular/common/http';
+
+import { WishModule } from './wish/wish.module';
 
 @NgModule({
   //components / directives that belong to this model
   declarations: [
-    AppComponent,
-    WishListComponent,
-    AddWishFormComponent,
-    WishFilterComponent,
-    WishListItemComponent
+    AppComponent
   ],
   //things avabile to the templates inside of this module
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    HttpClientModule
+    WishModule
   ],
   //objects that can be injected to parts of this model
   providers: [
