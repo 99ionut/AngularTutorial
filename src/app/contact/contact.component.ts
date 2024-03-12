a13b0703a0aea50fb19b492ab2dc2bfda7a0f6a3
Ionut Suciu <suciuionut888@gmail.com>
2024-03-12 16:46:36 +0100

    how to validating form fields

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -10,14 +10,15 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class ContactComponent {
 
   contactForm = new FormGroup({
-    senderName: new FormControl(""),
-    senderEmail: new FormControl(""),
-    senderMessage: new FormControl(""),
+    senderName: new FormControl("", Validators.required),
+    senderEmail: new FormControl("", [Validators.required, Validators.email]),
+    senderMessage: new FormControl("", [Validators.required, Validators.minLength(10)]),
   });
 
   
 
   submitForm(){
+    console.log(this.contactForm.valid);
     console.log(this.contactForm.value);
   }
 }
