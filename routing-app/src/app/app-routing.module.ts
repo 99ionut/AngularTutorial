15879e10795ddd944f559e86ac64577a182cc627
Ionut Suciu <suciuionut888@gmail.com>
2024-03-13 11:58:07 +0100

    routing with link or programatically with function

--- routing-app/src/app/app-routing.module.ts
+++ routing-app/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ContactComponent } from './contact/contact.component';
 
 //sThe order of this matters!!!! if you put {path: '**', component: NotFoundComponent}, before {path: 'second', component: SecondComponent}, its gonna match '**'
 const routes: Routes = [
   {path: '', component: FirstComponent},
   {path: 'first', component: FirstComponent},
   {path: 'second', component: SecondComponent},
+  {path: 'contact', component: ContactComponent},
   {path: '**', component: NotFoundComponent},
 ];
 
