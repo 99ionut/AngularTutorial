9be46b792280eaef9e35480ee2e4cc6c5058f087
Ionut Suciu <suciuionut888@gmail.com>
2024-03-13 11:41:20 +0100

    handling not found paths, order matters

--- routing-app/src/app/app-routing.module.ts
+++ routing-app/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-//setup default path by using ''
+//sThe order of this matters!!!! if you put {path: '**', component: NotFoundComponent}, before {path: 'second', component: SecondComponent}, its gonna match '**'
 const routes: Routes = [
   {path: '', component: FirstComponent},
   {path: 'first', component: FirstComponent},
   {path: 'second', component: SecondComponent},
+  {path: '**', component: NotFoundComponent},
 ];
 
 @NgModule({
